3177cc4f2fc177b6772cf6e074f3d2604a13f86d
Nicolas Guilbert <nicolas@ange.dk>
2016-12-26 00:42:46 +0100

    Range finder test

---
+++ rangefindertest/rangefindertest.ino
@@ -0,0 +1,21 @@
+int inputPin  = 12;    // define pin for sensor echo
+int outputPin = 13;    // define pin for sensor trig
+
+
+void setup() {
+    Serial.begin(9600);     // Define motor output pin
+    pinMode(inputPin, INPUT);    // define input pin for sensor
+    pinMode(outputPin, OUTPUT);  // define output pin for sensor  
+}
+
+void loop() {
+    digitalWrite(outputPin, LOW);   // ultrasonic sensor transmit low level signal 2μs
+    delayMicroseconds(2);
+    digitalWrite(outputPin, HIGH);  // ultrasonic sensor transmit high level signal10μs, at least 10μs
+    delayMicroseconds(10);
+    digitalWrite(outputPin, LOW);    // keep transmitting low level signal
+    float pingTime = pulseIn(inputPin, HIGH);  // read the time in between
+    Serial.print("pingTime: ");
+    Serial.print(pingTime);
+    Serial.print("\n");
+}
